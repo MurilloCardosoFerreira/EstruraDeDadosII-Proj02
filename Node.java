ed2105bea726ba9352a9d2a9c23bc51e91f4183d
Alexandre Luppi <124376713+AlexandreLSevero@users.noreply.github.com>
2024-11-15 18:42:32 -0300

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,1 +1,15 @@
 
+public class Node {
+    int key; // Chave de ordenação
+    Object data; // Dados associados
+    Node left, right; // Filhos
+    int height; // Apenas para AVL
+
+    public Node(int key, Object data) {
+        this.key = key;
+        this.data = data;
+        this.left = null;
+        this.right = null;
+        this.height = 1; // Altura inicial
+    }
+}
